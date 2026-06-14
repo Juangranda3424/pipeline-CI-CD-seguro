ccd75bc18bb33f2800d7d632dadacb2c210832fc
Juangranda3424 <juan.granda3424@gmail.com>
2026-06-14 10:48:51 -0500

    prueba en workflow para automatizar el merge de dev a test cuando pasa todas la pruebas

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/usuarios", router);
 
+// Ruta de prueba para verificar que la API está funcionando
 app.get("/", (req, res) => {
     res.json({ mensaje: "API funcionando" });
 });
